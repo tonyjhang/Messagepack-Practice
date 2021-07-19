9acd5bdf956dfacfaf22d8fc3cc1f03316c4145f
Tony Jhang <tony40815@gmail.com>
2021-07-20 03:50:12 +0800

    test: add pack test case

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sys
 
 res = {
     "a list": [1, 42, 3.141, 1337, "help"],
-    "a string": "bla",
-    "another dict": {"foo": "bar", "key": "value", "the answer": 42}
+    "a string": None,
+    "another dict": {"foo": True, "key": "value", "the answer": 42}
 }
 packed_res = pack(res)
-print(sys.getsizeof(packed_res), packed_res)
+print(f'Packed size:{sys.getsizeof(packed_res)}, data: {packed_res}')
